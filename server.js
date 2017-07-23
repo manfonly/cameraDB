4251c4198876ebe98ac2a9667d627d238805f103
yiymeng <yiymeng@cisco.com>
2017-07-23 10:03:24 +0800

    Add error page

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ mongoose.connect('mongodb://localhost/toy');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
 
 var routes = require('./api/routes/cameraDBRoutes');
 routes(app);
